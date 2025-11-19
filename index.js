1d92d8b01faff7627ce241d8c61b0e95fa0f3362
Zairaxx <brandontsegai@gmail.com>
2025-11-19 14:23:46 +0100

    added dynamic selectors and event.target example

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-function changeBox(event){
-
-    //event.target är alltid elementet som triggade igång eventet dvs <button>
-    console.log(event.target);
-        //H2 förekommer ovanför knappen i HTML, så vi kan nå den på följande sätt.
-        let h2 = event.target.previousElementSibling;
-
-        h2.style.color = "black";
-
-        // Div är dess parent
-        let bigBox = event.target.parentElement;
-        
-        bigBox.style.background = "darkred";
-
-        //För att ta bort hela div:en.
-        // bigBox.remove();
-        
-}
+//Lägga till-knappen
+const addStudentBtn = document.querySelector("#addStudent");
+//Dropdown-input
+const educationInput = document.querySelector("#education")
+//Namn på elev-input
+const studentNameInput = document.querySelector("#studentName");
 
 
-//Lägger till samma funktion till samtliga knappar i dokumentet.
+addStudentBtn.addEventListener("click", () => {
+    
+    let klass = educationInput.value;
 
-let allBtns = document.querySelectorAll("button");
+    //Skapa upp list-item med elevens namn
+    let li = document.createElement("li");
+    li.innerText = studentNameInput.value;
+    
+    //Skapa delete-knapp
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Radera"
+    li.append(deleteBtn);
 
-allBtns.forEach((btn) => {
+    deleteBtn.addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+    })
 
-    btn.addEventListener("click", changeBox)
-
+    //Dynamisk selector
+    document.querySelector(`#${klass}-list`).append(li);
+    
 })
