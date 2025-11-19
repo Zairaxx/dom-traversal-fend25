bda8fcace1e421b99369a692fdde91a7d23a0e77
Zairaxx <brandontsegai@gmail.com>
2025-11-19 11:24:40 +0100

    commented and made small changes for clarity

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-let allBtns = document.querySelectorAll("button");
+function changeBox(event){
 
-function deleteBox(event){
-    console.log("Hej");
-        // H2 är dess sibling
-        // let h2 = event.target.previousElementSibling;
+    //event.target är alltid elementet som triggade igång eventet dvs <button>
+    console.log(event.target);
+        //H2 förekommer ovanför knappen i HTML, så vi kan nå den på följande sätt.
+        let h2 = event.target.previousElementSibling;
 
-        // // Div är dess parent
-        // let bigBox = event.target.parentElement;
+        h2.style.color = "black";
+
+        // Div är dess parent
+        let bigBox = event.target.parentElement;
+        
+        bigBox.style.background = "darkred";
+
+        //För att ta bort hela div:en.
         // bigBox.remove();
         
-        // console.log(event.target.parentElement);
-
 }
+
+
+//Lägger till samma funktion till samtliga knappar i dokumentet.
+
+let allBtns = document.querySelectorAll("button");
 
 allBtns.forEach((btn) => {
 
-    btn.addEventListener("click", deleteBox)
+    btn.addEventListener("click", changeBox)
 
 })
